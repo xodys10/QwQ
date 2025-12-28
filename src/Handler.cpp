381b4ad8021330010b3e6a06e83a2f7d29913c1a
zVaiku <joshreed1ee@gmail.com>
2025-12-28 17:32:51 -0500

    fix templates via Handler.cpp

--- src/Handler.cpp
+++ src/Handler.cpp
@@ -50,29 +50,7 @@ void logarithm::logCstr(const char* str) {
     }
 }
 
-template<typename T, typename... Args>
-void logarithm::formatImpl(const char* fmt, T val, Args... args) {
-    while (*fmt) {
-        if (fmt[0] == '{' && fmt[1] == ':' && fmt[3] == '}') {
-            char spec = fmt[2];
-            switch (spec) {
-                case 'd': logInt(static_cast<int>(val)); break;
-                case 'x': logHex(static_cast<uintptr_t>(val)); break;
-                case 'p': logHex(reinterpret_cast<uintptr_t>(val)); break;
-                case 's': logCstr(static_cast<const char*>(val)); break;
-                default: std::fputc('?', stdout); break;
-            }
-
-            fmt += 4;
-            if constexpr(sizeof...(args) > 0)
-                formatImpl(fmt, args...);
-            return;
-        } else {
-            std::fputc(*fmt, stdout);
-        }
-        ++fmt;
-    }
-}
+// templated functions moved to Template.hpp
 
 void logarithm::formatImpl(const char* fmt) {
     while (*fmt) {
@@ -81,15 +59,7 @@ void logarithm::formatImpl(const char* fmt) {
     }   
 }
 
-template<typename... Args>
-void logarithm::logFmt(LOG_LEVEL level, const char* fmt, Args... args) {
-    if (level > logLevel) return;
-
-    printf("[%s]: ", toString(level));
-    formatImpl(fmt, args...);
-    std::fputc('\n', stdout);
-}
-
-// see Api.cpp
+// templated functions moved to Template.hpp
+// see Api.cpp for user usage
 
 } // namespace log
